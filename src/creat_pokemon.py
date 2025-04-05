f245090a559e1f0c4095b1ebaa2b807a9d1a96ed
Theo-Lempereur <theo.lempereur@student.junia.com>
2025-04-05 22:29:25 +0200

    delete le mauvais

--- src/creat_pokemon.py
+++
@@ -1,5 +0,0 @@
-#creat a pokemon with the name and load his informations
-
-#creat a move with the right name
-
-#Add a move to a pokemon. 
---
+++ src/create_pokemon.py
@@ -0,0 +1,5 @@
+#creat a pokemon with the name and load his informations
+
+#creat a move with the right name
+
+#Add a move to a pokemon. 

cb60772a6d142a5e94a41d2011bd0bce0b4de198
jules-levecq <144238687+jules-levecq@users.noreply.github.com>
2025-04-06 02:26:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-from src.data_loader import read_csv_data
 from src.create_pokemon import PokemonFactory
 from src.pokemon_dammage_calculator import PokemonDamageCalculator
 
-# Initialisation de la factory et du calculateur
+# Initialize the factory and the damage calculator
 factory = PokemonFactory('data/pokemon.csv', 'data/moves.csv')
 damage_calculator = PokemonDamageCalculator('data/chart.csv')
 
-# Création des Pokémon
+# Create Pokémon
 pikachu = factory.create_pokemon("Pikachu")
 bulbasaur = factory.create_pokemon("Bulbasaur")
 
-# Ajout des attaques
+# Add moves
 factory.add_move_to_pokemon(pikachu, "Thunderbolt")
 factory.add_move_to_pokemon(bulbasaur, "Vine Whip")
 
-# Calcul des dégâts
+# Calculate damage
 damage = damage_calculator.calculate_damage(pikachu, bulbasaur, pikachu.moves[0])
 
-# Affichage
-print(f"{pikachu.name} inflige {damage:.2f} dégâts à {bulbasaur.name} avec l'attaque {pikachu.moves[0].name}.")
+# Display the result
+print(f"{pikachu.name} deals {damage:.2f} damage to {bulbasaur.name} using {pikachu.moves[0].name}.")
